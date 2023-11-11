159551f40f4b236c090fee9b448c08bd8d696056
YanochkaSp <sharmgala@mail.ru>
2023-11-11 14:22:11 +0300

    divided into files

---
+++ task_4/stack.h
@@ -0,0 +1,24 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Stack
+{
+    int* data;
+    int size;
+    int capacity;
+};
+
+void StackCtor (Stack* pStk, int initCapacity);
+
+void StackPush (Stack *pStk, int el);
+
+int StackPop (Stack *pStk);
+
+void StackDtor (Stack *pStk);
+
+void StackPrinter (Stack *pStk);
+
+#endif
